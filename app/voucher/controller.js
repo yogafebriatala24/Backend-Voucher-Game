11e9087ae276907227fcbc6b10be67bf2f96b44f
Yoga Febriatala <yogafebriatala24@gmail.com>
2022-06-01 16:26:30 +0700

    CRUD voucher

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -12,7 +12,9 @@ module.exports = {
       const alertStatus = req.flash("alertStatus");
 
       const alert = { message: alertMessage, status: alertStatus };
-      const voucher = await Voucher.find();
+      const voucher = await Voucher.find()
+        .populate("category")
+        .populate("nominals");
 
       res.render("admin/voucher/view_voucher", {
         alert,
@@ -92,59 +94,117 @@ module.exports = {
     }
   },
 
-  //   viewEdit: async (req, res) => {
-  //     try {
-  //       const { id } = req.params;
-  //       const nominal = await Nominal.findOne({ _id: id });
+  viewEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const category = await Category.find();
+      const nominal = await Nominal.find();
+      const voucher = await Voucher.findOne({ _id: id })
+        .populate("category")
+        .populate("nominals");
 
-  //       res.render("admin/nominal/edit", {
-  //         nominal,
-  //       });
-  //     } catch (err) {
-  //       req.flash("alertMessage", `${err.message}`);
-  //       req.flash("alertStatus", "danger");
-  //       res.redirect("/nominal");
-  //     }
-  //   },
+      res.render("admin/voucher/edit", {
+        voucher,
+        nominal,
+        category,
+      });
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/voucher");
+    }
+  },
 
-  //   actionEdit: async (req, res) => {
-  //     try {
-  //       const { id } = req.params;
-  //       const { coinName, coinQuantity, price } = req.body;
+  actionEdit: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const { name, category, nominals } = req.body;
+      if (req.file) {
+        let tmp_path = req.file.path;
+        let originaExt =
+          req.file.originalname.split(".")[
+            req.file.originalname.split(".").length - 1
+          ];
+        let filename = req.file.filename + "." + originaExt;
+        let target_path = path.resolve(
+          config.rootPath,
+          `public/uploads/${filename}`
+        );
+        const src = fs.createReadStream(tmp_path);
+        const dest = fs.createWriteStream(target_path);
 
-  //       await Nominal.findOneAndUpdate(
-  //         {
-  //           _id: id,
-  //         },
-  //         { coinName, coinQuantity, price }
-  //       );
+        src.pipe(dest);
+        src.on("end", async () => {
+          try {
+            const voucher = await Voucher.findOne({ _id: id });
+            let currentImage = `${config.rootPath}/public/uploads/${voucher.thumbnial}`;
+            if (fs.existsSync(currentImage)) {
+              fs.unlinkSync(currentImage);
+            }
 
-  //       req.flash("alertMessage", "Berhasil Edit Nominal");
-  //       req.flash("alertStatus", "success");
-  //       res.redirect("/nominal");
-  //     } catch (err) {
-  //       req.flash("alertMessage", `${err.message}`);
-  //       req.flash("alertStatus", "danger");
-  //       res.redirect("/nominal");
-  //     }
-  //   },
+            await Voucher.findOneAndUpdate(
+              {
+                _id: id,
+              },
+              {
+                name,
+                category,
+                nominals,
+                thumbnial: filename,
+              }
+            );
+            await voucher.save();
+            req.flash("alertMessage", "Berhasil Ubah Voucher");
+            req.flash("alertStatus", "success");
+            res.redirect("/voucher");
+          } catch (err) {
+            req.flash("alertMessage", `${err.message}`);
+            req.flash("alertStatus", "danger");
+            res.redirect("/voucher");
+          }
+        });
+      } else {
+        await Voucher.findOneAndUpdate(
+          {
+            _id: id,
+          },
+          {
+            name,
+            category,
+            nominals,
+          }
+        );
+      }
+      req.flash("alertMessage", "Berhasil Ubah Voucher");
+      req.flash("alertStatus", "success");
+      res.redirect("/voucher");
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/voucher");
+    }
+  },
 
-  //   actionDelete: async (req, res) => {
-  //     try {
-  //       const { id } = req.params;
+  actionDelete: async (req, res) => {
+    try {
+      const { id } = req.params;
 
-  //       await Nominal.findOneAndRemove({
-  //         _id: id,
-  //       });
+      const voucher = await Voucher.findOneAndRemove({
+        _id: id,
+      });
+      let currentImage = `${config.rootPath}/public/uploads/${voucher.thumbnial}`;
+      if (fs.existsSync(currentImage)) {
+        fs.unlinkSync(currentImage);
+      }
 
-  //       req.flash("alertMessage", "Berhasil Hapus Nominal");
-  //       req.flash("alertStatus", "success");
+      req.flash("alertMessage", "Berhasil Hapus Voucher");
+      req.flash("alertStatus", "success");
 
-  //       res.redirect("/nominal");
-  //     } catch (err) {
-  //       req.flash("alertMessage", `${err.message}`);
-  //       req.flash("alertStatus", "danger");
-  //       res.redirect("/nominal");
-  //     }
-  //   },
+      res.redirect("/voucher");
+    } catch (err) {
+      req.flash("alertMessage", `${err.message}`);
+      req.flash("alertStatus", "danger");
+      res.redirect("/voucher");
+    }
+  },
 };
--- app/voucher/router.js
+++ app/voucher/router.js
@@ -4,9 +4,9 @@ const {
   index,
   viewCreate,
   actionCreate,
-  //   viewEdit,
-  //   actionEdit,
-  //   actionDelete,
+  viewEdit,
+  actionEdit,
+  actionDelete,
 } = require("./controller");
 const multer = require("multer");
 const os = require("os");
@@ -19,8 +19,12 @@ router.post(
   multer({ dest: os.tmpdir() }).single("image"),
   actionCreate
 );
-// router.get("/edit/:id", viewEdit);
-// router.put("/edit/:id", actionEdit);
-// router.delete("/delete/:id", actionDelete);
+router.get("/edit/:id", viewEdit);
+router.put(
+  "/edit/:id",
+  multer({ dest: os.tmpdir() }).single("image"),
+  actionEdit
+);
+router.delete("/delete/:id", actionDelete);
 
 module.exports = router;
