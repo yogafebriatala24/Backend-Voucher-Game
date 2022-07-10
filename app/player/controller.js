5c458d7fd2ca3e7834138583b08abe588868fd5c
Yoga Febriatala <yogafebriatala24@gmail.com>
2022-07-11 00:50:04 +0700

    api detail page

--- app/player/controller.js
+++ app/player/controller.js
@@ -10,9 +10,26 @@ module.exports = {
         .populate("category");
       res.status(200).json({ data: voucher });
     } catch (err) {
-      res
-        .status(500)
-        .json({ message: err.message || `Terjadi kesalahan pada server` });
+      res.status(500).json({ message: err.message || `Internal server error` });
+    }
+  },
+  detailPage: async (req, res) => {
+    try {
+      const { id } = req.params;
+      const voucher = await Voucher.findOne({ _id: id })
+        .populate("category")
+        .populate("nominals")
+        .populate("user", "_id name phoneNumber");
+
+      if (!voucher) {
+        return res
+          .status(404)
+          .json({ message: "voucher game tidak ditemukan.!" });
+      }
+
+      res.status(200).json({ data: voucher });
+    } catch (err) {
+      res.status(500).json({ message: err.message || `Internal server error` });
     }
   },
 };
